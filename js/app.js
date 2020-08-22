510f69ed5d44241e3a0ef4b2ee69dff261e127a8
coder236 <coder236@gmail.com>
2020-08-22 16:47:15 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //Select the wrapper in html
+    $('.S5slider').slick({
+        arrows:false, //It results in hidding prev and next buttons
+        dots:true, //Now dots will be displayed
+        appendDots:'.S5sliderdots',// It tells where the dots must be shown
+        dotsClass:'S5dots'//no . becoz it is custom class
+    });
+});
+
+
+/* For toggle */
+function myFunction(event) {
+  event.stopPropagation();
+  document.getElementById("myDropdown").classList.toggle("open");
+}
+window.onclick = function(event) {
+  document.getElementById("myDropdown").classList.remove("open");
+}
+  
