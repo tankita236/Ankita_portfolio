490d75b848195e769f2a7b8c9835751b86f55b6a
coder236 <coder236@gmail.com>
2020-08-23 23:31:10 +0530

    Added lazy loading for Index Page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    //Select the wrapper in html
-    $('.S5slider').slick({
-        arrows:false, //It results in hidding prev and next buttons
-        dots:true, //Now dots will be displayed
-        appendDots:'.S5sliderdots',// It tells where the dots must be shown
-        dotsClass:'S5dots'//no . becoz it is custom class
-    });
+$(document).ready(function () {
+  //Select the wrapper in html
+  $('.S5slider').slick({
+    arrows: false, //It results in hidding prev and next buttons
+    dots: true, //Now dots will be displayed
+    appendDots: '.S5sliderdots',// It tells where the dots must be shown
+    dotsClass: 'S5dots'//no . becoz it is custom class
+  });
 });
 
 
@@ -14,7 +14,38 @@ function myFunction(event) {
   event.stopPropagation();
   document.getElementById("myDropdown").classList.toggle("open");
 }
-window.onclick = function(event) {
+window.onclick = function (event) {
   document.getElementById("myDropdown").classList.remove("open");
 }
-  
+
+
+// Lazy Loading
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      var scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < (window.innerHeight + scrollTop)) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+        }
+      });
+      if (lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
+      }
+    }, 20);
+  }
+
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
