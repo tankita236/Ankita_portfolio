7d0d70b839036ebb0507e410a1914165b32efe81
tankita236 <coder236@gmail.com>
2020-09-26 13:25:48 +0530

    removed lazy loading of image in home page

--- js/app.js
+++ js/app.js
@@ -17,35 +17,3 @@ function myFunction(event) {
 window.onclick = function (event) {
   document.getElementById("myDropdown").classList.remove("open");
 }
-
-
-// Lazy Loading
-document.addEventListener("DOMContentLoaded", function () {
-  var lazyloadImages = document.querySelectorAll("img.lazy");
-  var lazyloadThrottleTimeout;
-
-  function lazyload() {
-    if (lazyloadThrottleTimeout) {
-      clearTimeout(lazyloadThrottleTimeout);
-    }
-
-    lazyloadThrottleTimeout = setTimeout(function () {
-      var scrollTop = window.pageYOffset;
-      lazyloadImages.forEach(function (img) {
-        if (img.offsetTop < (window.innerHeight + scrollTop)) {
-          img.src = img.dataset.src;
-          img.classList.remove('lazy');
-        }
-      });
-      if (lazyloadImages.length == 0) {
-        document.removeEventListener("scroll", lazyload);
-        window.removeEventListener("resize", lazyload);
-        window.removeEventListener("orientationChange", lazyload);
-      }
-    }, 20);
-  }
-
-  document.addEventListener("scroll", lazyload);
-  window.addEventListener("resize", lazyload);
-  window.addEventListener("orientationChange", lazyload);
-});
